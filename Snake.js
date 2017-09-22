aaa85c171842a3a7ddaed9b14c707d0722502748
MatthewFrench <matthewf@ksu.edu>
2017-09-22 10:46:20 -0500

    Added in-class logic.

--- Snake.js
+++ Snake.js
@@ -5,24 +5,67 @@ function Snake() {
   this.snake = [{x:7, y:7}, {x: 6, y:7}, {x: 5, y:7}];
   this.direction = 'right';
   this.food = [];
+  this.over = false;
 
-  var canvas = document.createElement('canvas');
+  let canvas = document.createElement('canvas');
   canvas.width = this.width * this.cellSize;
   canvas.height = this.height * this.cellSize;
   document.body.appendChild(canvas);
   this.ctx = canvas.getContext('2d');
 
-  window.onkeydown = this.handleKeyDown;
+  window.addEventListener('keydown', (event)=>{this.handleKeyDown(event)});
 
-  setInterval(() => {this.loop();}, 1000);
+  this.interval = setInterval(()=>this.loop(), 100);
 }
 
-Snake.prototype.handleKeyDown = function(event) {
 
+Snake.prototype.handleKeyDown = function(event) {
+  switch(event.key){
+    case 'w':
+    case 'ArrowUp':
+      this.direction = 'up';
+      break;
+    case 'a':
+    case 'ArrowLeft':
+      this.direction = 'left';
+      break;
+    case 's':
+    case 'ArrowDown':
+      this.direction = 'down';
+      break;
+    case 'd':
+    case 'ArrowRight':
+      this.direction = 'right';
+      break;
+  }
+};
+
+Snake.prototype.gameOver = function() {
+  clearInterval(this.interval);
+  window.removeEventListener('keydown', this.handleKeyDown);
+  window.addEventListener('keydown', ()=>{
+    new Snake();
+  }, {once: true});
+  this.over = true;
 };
 
 Snake.prototype.render = function() {
-  this.ctx.drawRect(0, 0, this.width * this.cellSize, this.height * this.cellSize);
+  if(this.over) {
+    this.ctx.fillStyle = 'rgba(255,0,0,0.25)';
+    this.ctx.fillRect(0,0,
+      this.width * this.cellSize,
+      this.height * this.cellSize);
+    this.ctx.fillStyle = "white";
+    this.ctx.font = '16px sans-serif';
+    this.ctx.fillText("Game Over", 10, 20);
+    this.ctx.fillText("Points: ", 10, 40);
+    this.ctx.font = '10px sans-serif';
+    this.ctx.fillText("Press any key for new game", 10, 60);
+    return;
+  }
+
+  this.ctx.fillStyle = 'black';
+  this.ctx.fillRect(0, 0, this.width * this.cellSize, this.height * this.cellSize);
 
   this.ctx.fillStyle = 'ivory';
   this.snake.forEach((segment) => {
@@ -37,25 +80,25 @@ Snake.prototype.render = function() {
 };
 
 Snake.prototype.update = function() {
-  var x = this.snake[0].x;
-  var y = this.snake[0].y;
-  switch() {
-    case: 'right':
+  let x = this.snake[0].x;
+  let y = this.snake[0].y;
+  switch(this.direction) {
+    case 'right':
       x++;
       break;
-    case: 'left':
+    case 'left':
       x--;
       break;
-    case: 'up':
+    case 'up':
       y--;
       break;
-    case: 'down':
+    case 'down':
       y++;
       break;
   }
-  if (x < 0 || x > this.width || y < 0 || y > this.height) {
-    return;
-  }
+  // If we move off-board, game is over
+  if(x < 0 || x > this.width || y < 0 || y > this.height)
+    return this.gameOver();
   this.snake.pop();
   this.snake.unshift({x: x, y: y});
 };
@@ -64,3 +107,5 @@ Snake.prototype.loop = function() {
   this.update();
   this.render();
 };
+
+new Snake();
